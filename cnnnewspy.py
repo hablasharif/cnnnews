46fe70f534cc7146b92782ce8fed65413236ab44
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-11-04 18:41:02 +0600

    Create cnnnewspy.py

---
+++ cnnnewspy.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+from lxml import html
+import re
+
+# Function to scrape and extract English text without punctuation and special symbols
+def scrape_article(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        tree = html.fromstring(response.text)
+        article_text = tree.xpath("/html/body/div[1]/section[3]/section[1]/section[1]/article/section/main//text()")
+        article_text = " ".join(article_text)
+        
+        # Remove punctuation, special symbols, and retain only English text
+        article_text = re.sub(r'[^a-zA-Z\s]', '', article_text)
+        
+        # Split the text into words and remove duplicates
+        words = article_text.split()
+        unique_words = list(set(words))
+        
+        return unique_words
+    except Exception as e:
+        return []
+
+# Streamlit app
+st.title("News Article Scraper")
+
+st.write("Enter URLs of news articles to scrape their content (one URL per line):")
+input_urls = st.text_area("URLs (One URL per line):", "")
+
+if st.button("Scrape Articles"):
+    urls = input_urls.strip().split("\n")
+    if urls:
+        unique_words_set = set()  # To store unique words across all articles
+        for url in urls:
+            article_words = scrape_article(url.strip())
+            unique_words_set.update(article_words)
+        
+        combined_content = ' '.join(unique_words_set)  # Combine unique words from all articles
+        st.text_area("Combined Unique English Text", combined_content)
+    else:
+        st.warning("Please enter one or more valid URLs.")
+
+st.text("Note: This is a basic web scraping example for educational purposes.")
