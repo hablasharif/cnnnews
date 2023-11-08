65d9dfd87686728e15ac6df04df9044fa35a8edd
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-11-09 01:18:57 +0600

    Update cnnnewspy.py

--- cnnnewspy.py
+++ cnnnewspy.py
@@ -1,28 +1,27 @@
 import streamlit as st
-import requests
-from lxml import html
+import httpx
+import lxml.html as lh
 import re
 
 # Function to scrape and extract English text without punctuation and special symbols
-def scrape_article(url):
+async def scrape_article_async(url, session):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        tree = html.fromstring(response.text)
-        paragraphs = tree.xpath("//p")
-        
-        article_text = ""
-        for p in paragraphs:
-            article_text += p.text_content() + " "
+        async with session.get(url) as response:
+            response.raise_for_status()
+            text = await response.text()
 
-        # Remove punctuation, special symbols, and retain only English text
-        article_text = re.sub(r'[^a-zA-Z\s]', '', article_text)
-        
-        # Split the text into words and remove duplicates
-        words = article_text.split()
-        unique_words = list(set(words))
-        
-        return unique_words
+            tree = lh.fromstring(text)
+            paragraphs = tree.xpath("//p")
+
+            article_text = ""
+            for p in paragraphs:
+                article_text += p.text_content() + " "
+
+            article_text = re.sub(r'[^a-zA-Z\s]', '', article_text)
+            words = article_text.split()
+            unique_words = list(set(words))
+
+            return unique_words
     except Exception as e:
         return []
 
@@ -36,13 +35,17 @@ if st.button("Scrape Articles"):
     urls = input_urls.strip().split("\n")
     if urls:
         unique_words_set = set()  # To store unique words across all articles
-        for url in urls:
-            article_words = scrape_article(url.strip())
-            unique_words_set.update(article_words)
-        
+
+        async with httpx.AsyncClient() as session:
+            tasks = [scrape_article_async(url.strip(), session) for url in urls]
+            article_words = await asyncio.gather(*tasks)
+
+            for words in article_words:
+                unique_words_set.update(words)
+
         combined_content = ' '.join(unique_words_set)  # Combine unique words from all articles
         st.text_area("Combined Unique English Text", combined_content)
     else:
         st.warning("Please enter one or more valid URLs.")
 
-st.text("Note: This is a basic web scraping example for educational purposes.")
+st.text("Note: This is an asynchronous web scraping example for educational purposes.")
