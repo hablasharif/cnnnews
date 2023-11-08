299804ff61027742fb22f8c3c5c4c5365fc92588
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-11-09 01:31:17 +0600

    Update cnnnewspy.py

--- cnnnewspy.py
+++ cnnnewspy.py
@@ -1,28 +1,33 @@
 import streamlit as st
+import concurrent.futures
 import requests
-from lxml import html
-import re
+from bs4 import BeautifulSoup
+import string
 
 # Function to scrape and extract English text without punctuation and special symbols
 def scrape_article(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        tree = html.fromstring(response.text)
-        paragraphs = tree.xpath("//p")
-        
-        article_text = ""
-        for p in paragraphs:
-            article_text += p.text_content() + " "
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Remove punctuation, special symbols, and retain only English text
-        article_text = re.sub(r'[^a-zA-Z\s]', '', article_text)
-        
-        # Split the text into words and remove duplicates
-        words = article_text.split()
-        unique_words = list(set(words))
-        
-        return unique_words
+        # Find the relevant HTML element containing the article content
+        article_element = soup.find('div', {'class': 'article-content'})  # Adjust as needed
+
+        if article_element:
+            article_text = article_element.get_text()
+
+            # Remove punctuation, special symbols, and retain only English text
+            translator = str.maketrans('', '', string.punctuation)
+            article_text = article_text.translate(translator)
+
+            # Split the text into words and remove duplicates
+            words = article_text.split()
+            unique_words = list(set(words))
+
+            return unique_words
+        else:
+            return []
     except Exception as e:
         return []
 
@@ -36,11 +41,14 @@ if st.button("Scrape Articles"):
     urls = input_urls.strip().split("\n")
     if urls:
         unique_words_set = set()  # To store unique words across all articles
-        for url in urls:
-            article_words = scrape_article(url.strip())
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = executor.map(scrape_article, urls)
+
+        for article_words in results:
             unique_words_set.update(article_words)
-        
+
         combined_content = ' '.join(unique_words_set)  # Combine unique words from all articles
         st.text_area("Combined Unique English Text", combined_content)
     else:
-        st.warning("Please enter one or more valid URLs.") 
+        st.warning("Please enter one or more valid URLs.")
