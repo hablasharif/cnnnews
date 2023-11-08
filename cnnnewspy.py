0973b22cb8cf2637ecad1f9de02ffa3e59fff456
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-11-09 01:19:56 +0600

    Update cnnnewspy.py

--- cnnnewspy.py
+++ cnnnewspy.py
@@ -2,6 +2,7 @@ import streamlit as st
 import httpx
 import lxml.html as lh
 import re
+import asyncio
 
 # Function to scrape and extract English text without punctuation and special symbols
 async def scrape_article_async(url, session):
@@ -31,21 +32,25 @@ st.title("News Article Scraper")
 st.write("Enter URLs of news articles to scrape their content (one URL per line):")
 input_urls = st.text_area("URLs (One URL per line):", "")
 
-if st.button("Scrape Articles"):
-    urls = input_urls.strip().split("\n")
-    if urls:
-        unique_words_set = set()  # To store unique words across all articles
+async def main():
+    if st.button("Scrape Articles"):
+        urls = input_urls.strip().split("\n")
+        if urls:
+            unique_words_set = set()  # To store unique words across all articles
 
-        async with httpx.AsyncClient() as session:
-            tasks = [scrape_article_async(url.strip(), session) for url in urls]
-            article_words = await asyncio.gather(*tasks)
+            async with httpx.AsyncClient() as session:
+                tasks = [scrape_article_async(url.strip(), session) for url in urls]
+                article_words = await asyncio.gather(*tasks)
 
-            for words in article_words:
-                unique_words_set.update(words)
+                for words in article_words:
+                    unique_words_set.update(words)
 
-        combined_content = ' '.join(unique_words_set)  # Combine unique words from all articles
-        st.text_area("Combined Unique English Text", combined_content)
-    else:
-        st.warning("Please enter one or more valid URLs.")
+            combined_content = ' '.join(unique_words_set)  # Combine unique words from all articles
+            st.text_area("Combined Unique English Text", combined_content)
+        else:
+            st.warning("Please enter one or more valid URLs.")
 
 st.text("Note: This is an asynchronous web scraping example for educational purposes.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
